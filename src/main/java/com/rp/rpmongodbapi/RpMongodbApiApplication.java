f88c2594965cd413aa430f8af73663540700c176
rpatel1291 <rpatel1291@gmail.com>
2021-01-24 13:31:21 -0500

    Initial setup
    Commit for GIt init, Spring init project with dependencies for Mongo, Spring boot, lombok

---
+++ src/main/java/com/rp/rpmongodbapi/RpMongodbApiApplication.java
@@ -0,0 +1,13 @@
+package com.rp.rpmongodbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpMongodbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpMongodbApiApplication.class, args);
+	}
+
+}
